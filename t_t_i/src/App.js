0935dc962a2400b3541e89bfd8784c0a4c858e82
NhatHuy1812 <nhathuynguyen1812@gmail.com>
2023-09-21 17:34:23 +0700

    API TEXT TO IMAGE

---
+++ t_t_i/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const API_TOKEN = "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";
+
+const ImageGenerationForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [output, setOutput] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const input = event.target.elements.input.value;
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/prompthero/openjourney",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+        body: JSON.stringify({ inputs: input }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to generate image");
+    }
+
+    const blob = await response.blob();
+    setOutput(URL.createObjectURL(blob));
+    setLoading(false);
+  };
+
+  return (<div className="container al-c mt-3">
+    <h1>TEXT<span> TO </span>IMAGE</h1>
+    <p>Type test</p>
+    <form className="gen-form" onSubmit={handleSubmit}>
+      <input type="text" name="input"/>
+      <button type="submit">Gen</button>
+    </form>
+    <div>
+    {loading && <div className="loading">Loading...</div>}
+    {!loading && output && (
+      <div className="result-image">
+        <img src={output} alt="art"  />
+      </div>
+    )}
+    </div>
+
+    </div>);
+  
+};
+
+export default ImageGenerationForm;
